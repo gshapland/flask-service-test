e4b244fbdc52959ee94e5c41776bf173def4dfec
gshapland <gshapland@situationalcorp.com>
2016-10-12 17:57:58 -0700

    Add test service.

---
+++ service.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
